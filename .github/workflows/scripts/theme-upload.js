4861f9e765527c8ae82798e577983b9b4f72bb5b
Vladimir <guerramacias.vladimir@gmail.com>
2021-02-21 20:59:04 -0700

    update port

--- .github/workflows/scripts/theme-upload.js
+++ .github/workflows/scripts/theme-upload.js
@@ -18,7 +18,7 @@ const {
 
 const uploadTheme = async () => {
     try {
-        const port = 80;
+        const port = 51671;
         const params = {
             port, // Defaults to 8080
             open: false, // don't load browser
