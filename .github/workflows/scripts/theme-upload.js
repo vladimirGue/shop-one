a513f00e6e1677b7bc755298621e1a2f5419827d
Vladimir <guerramacias.vladimir@gmail.com>
2021-02-22 20:53:12 -0700

    full

--- .github/workflows/scripts/theme-upload.js
+++ .github/workflows/scripts/theme-upload.js
@@ -18,7 +18,7 @@ const {
 
 const uploadTheme = async () => {
     try {
-        const port = 22;
+        const port = 8181;
         const params = {
             port, // Defaults to 8080
             host: "0.0.0.0",
@@ -31,7 +31,7 @@ const uploadTheme = async () => {
 
         const ngrokUrl = await ngrok.connect({
             authtoken: NGROK_AUTH_TOKEN,
-            port,
+            addr:8181,
         });
 
         const data = prData.getPrData();
