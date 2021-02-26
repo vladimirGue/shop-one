a3b3fee207b4efcf74c65a27ad85ad8a703f55d7
Vladimir <guerramacias.vladimir@gmail.com>
2021-02-26 00:27:30 -0700

    full

--- .github/workflows/scripts/lib/pr-data.js
+++ .github/workflows/scripts/lib/pr-data.js
@@ -9,9 +9,15 @@ const {
 const octokit = new Octokit({ auth: GITHUB_AUTH_TOKEN });
 
 module.exports.getPrData = async() => {
-    const event_path= await octokit.request('GET /repos/{owner}/{repo}/pulls/', {
-        owner: 'vladimirGue',
-        repo: 'shop-one',
-      });
-    return JSON.parse(fs.readFileSync(event_path));
+    try {
+        const event_path= await octokit.request('GET /repos/{owner}/{repo}/pulls/', {
+            owner: 'vladimirGue',
+            repo: 'shop-one',
+          });
+        return JSON.parse(fs.readFileSync(event_path));
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+    
 };
