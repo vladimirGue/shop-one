8fa9cbe72187a9480a3b0537273c578bfe3050fe
Vladimir <guerramacias.vladimir@gmail.com>
2021-02-25 23:29:20 -0700

    full

--- .github/workflows/scripts/lib/pr-data.js
+++ .github/workflows/scripts/lib/pr-data.js
@@ -6,7 +6,7 @@ const {
 } = process.env;
 
 module.exports.getPrData = async() => {
-    const event_path= await octokit.request('https://api.github.com/repos/{owner}/{repo}/pulls/', {
+    const event_path= await octokit.request('POST /repos/{owner}/{repo}/pulls/', {
         owner: 'vladimirGue',
         repo: 'shop-one',
         head: 'head',
