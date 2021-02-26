5cfa26523dde5034373276754ba7cc926ea2d48e
Vladimir <guerramacias.vladimir@gmail.com>
2021-02-25 23:45:44 -0700

    full

--- .github/workflows/scripts/lib/pr-data.js
+++ .github/workflows/scripts/lib/pr-data.js
@@ -1,16 +1,16 @@
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 const fs = require('fs');
 require('dotenv').config();
 
 const {
     GITHUB_EVENT_PATH,
 } = process.env;
+const octokit = new Octokit({ auth: `eba18bb794e016f37a1763ae91bd13dd7c27bcf3` });
 
 module.exports.getPrData = async() => {
-    const event_path= await octokit.request('POST /repos/{owner}/{repo}/pulls/', {
+    const event_path= await octokit.request('GET /repos/{owner}/{repo}/pulls/', {
         owner: 'vladimirGue',
         repo: 'shop-one',
-        head: 'head',
-        base: 'base'
       })
     return JSON.parse(fs.readFileSync(event_path));
 };
