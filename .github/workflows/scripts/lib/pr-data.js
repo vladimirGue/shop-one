c20928cec6536cb34f53fa5eb934a304bd3337e0
Vladimir <guerramacias.vladimir@gmail.com>
2021-03-09 11:48:16 -0700

    update pr-data

--- .github/workflows/scripts/lib/pr-data.js
+++ .github/workflows/scripts/lib/pr-data.js
@@ -1,29 +1,5 @@
-const { Octokit } = require("@octokit/core");
-const { createAppAuth } = require("@octokit/auth-app");
 const fs = require('fs');
-require('dotenv').config();
-
-const {
-    GITHUB_EVENT_PATH,
-    GITHUB_AUTH_TOKEN,
-} = process.env;
 
 module.exports.getPrData = () => {
-    //const appOctokit = new Octokit();
-  
-  //const { data } = await appOctokit.request("GET /repos/vladimirGue/shop-one/events");
   return JSON.parse(fs.readFileSync('/home/runner/work/_temp/_github_workflow/event.json'));
 };
-/*getPrData = async() => {
-    //return JSON.parse(fs.readFileSync('/home/runner/work/_temp/_github_workflow/event.json'));
-    try {
-        const octokit = new Octokit({ auth:"cd9e9f9cbce4742c495ec206c735d16e18f12481v",});
-        const data= await octokit.request("/user");
-        console.log(data);
-        //return data;//JSON.parse(fs.readFileSync(event_path));
-    } catch (e) {
-        console.log(e);
-        process.exit(1);
-    }
-    
-};*/
